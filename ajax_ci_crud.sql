45d8ab0efe20336ef5669bd34cc9cd686e9d994e
iyar99 <39215935+iyar99@users.noreply.github.com>
2018-05-27 10:39:44 +0700

    crud-ega

---
+++ ajax_ci_crud.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2018 at 06:02 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ajax_ci_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persons`
+--
+
+CREATE TABLE `persons` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `gender` enum('male','female') DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `dob` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `persons`
+--
+
+INSERT INTO `persons` (`id`, `firstName`, `lastName`, `gender`, `address`, `dob`) VALUES
+(1, 'Ega', 'Sherly', 'female', 'Bandung', '1996-12-18'),
+(6, 'Keisha', 'Aqilla', 'female', 'DIY', '2010-12-20'),
+(7, 'Julian', 'Dwi', 'male', 'Bandung Barat', '2003-07-07');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ application/views/person_view.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD</title>
+    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/datatables/css/dataTables.bootstrap.css')?>" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head> 
+  <body>
+
+  <div class="container">
+    <h3>Person Data</h3>
+    <br />
+    <button class="btn btn-success" onclick="add_person()"><i class="glyphicon glyphicon-plus"></i> Tambah</button>
+    <br />
+    <br />
+    <table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+      <thead>
+        <tr>
+          <th>Nama Depan</th>
+          <th>Nama Belakang</th>
+          <th>Jenis Kelamin</th>
+          <th>Alamat</th>
+          <th>Tanggal Lahir</th>
+          <th style="width:125px;">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+      
+    </table>
+	
+  </div>
+
+  <script src="<?php echo base_url('assets/jquery/jquery-2.1.4.min.js')?>"></script>
+  <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js')?>"></script>
+  <script src="<?php echo base_url('assets/datatables/js/jquery.dataTables.min.js')?>"></script>
+  <script src="<?php echo base_url('assets/datatables/js/dataTables.bootstrap.js')?>"></script>
+
+
+  <script type="text/javascript">
+
+    var save_method; //for save method string
+    var table;
+    $(document).ready(function() {
+      table = $('#table').DataTable({ 
+        
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('person/ajax_list')?>",
+            "type": "POST"
+        },
+
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        { 
+          "targets": [ -1 ], //last column
+          "orderable": false, //set not orderable
+        },
+        ],
+
+      });
+    });
+
+    function add_person()
+    {
+      save_method = 'add';
+      $('#form')[0].reset(); // reset form on modals
+      $('#modal_form').modal('show'); // show bootstrap modal
+      $('.modal-title').text('Tambah Baru'); // Set Title to Bootstrap modal title
+    }
+
+    function edit_person(id)
+    {
+      save_method = 'update';
+      $('#form')[0].reset(); // reset form on modals
+
+      //Ajax Load data from ajax
+      $.ajax({
+        url : "<?php echo site_url('person/ajax_edit/')?>/" + id,
+        type: "GET",
+        dataType: "JSON",
+        success: function(data)
+        {
+           
+            $('[name="id"]').val(data.id);
+            $('[name="firstName"]').val(data.firstName);
+            $('[name="lastName"]').val(data.lastName);
+            $('[name="gender"]').val(data.gender);
+            $('[name="address"]').val(data.address);
+            $('[name="dob"]').val(data.dob);
+            
+            $('#modal_form').modal('show'); // show bootstrap modal when complete loaded
+            $('.modal-title').text('Edit Data'); // Set title to Bootstrap modal title
+            
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Error get data from ajax');
+        }
+    });
+    }
+
+    function reload_table()
+    {
+      table.ajax.reload(null,false); //reload datatable ajax 
+    }
+
+    function save()
+    {
+      var url;
+      if(save_method == 'add') 
+      {
+          url = "<?php echo site_url('person/ajax_add')?>";
+      }
+      else
+      {
+        url = "<?php echo site_url('person/ajax_update')?>";
+      }
+
+       // ajax adding data to database
+          $.ajax({
+            url : url,
+            type: "POST",
+            data: $('#form').serialize(),
+            dataType: "JSON",
+            success: function(data)
+            {
+               //if success close modal and reload ajax table
+               $('#modal_form').modal('hide');
+               reload_table();
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert('Gagal menambahkan / update data');
+            }
+        });
+    }
+
+    function delete_person(id)
+    {
+      if(confirm('Yakin ingin menghapus data ini?'))
+      {
+        // ajax delete data to database
+          $.ajax({
+            url : "<?php echo site_url('person/ajax_delete')?>/"+id,
+            type: "POST",
+            dataType: "JSON",
+            success: function(data)
+            {
+               //if success reload ajax table
+               $('#modal_form').modal('hide');
+               reload_table();
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert('Gagal menghapus / update data');
+            }
+        });
+         
+      }
+    }
+
+  </script>
+
+  <!-- Bootstrap modal -->
+  <div class="modal fade" id="modal_form" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title">Tambah Data Baru</h3>
+      </div>
+      <div class="modal-body form">
+        <form action="#" id="form" class="form-horizontal">
+          <input type="hidden" value="" name="id"/> 
+          <div class="form-body">
+            <div class="form-group">
+              <label class="control-label col-md-3">Nama Depan</label>
+              <div class="col-md-9">
+                <input name="firstName" placeholder="Nama Depan" class="form-control" type="text">
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-md-3">Nama Belakang</label>
+              <div class="col-md-9">
+                <input name="lastName" placeholder="Nama Belakang" class="form-control" type="text">
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-md-3">Jenis Kelamin</label>
+              <div class="col-md-9">
+                <select name="gender" class="form-control">
+                  <option value="male">Laki-laki</option>
+                  <option value="female">Perempuan</option>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-md-3">Alamat</label>
+              <div class="col-md-9">
+                <textarea name="address" placeholder="Alamat"class="form-control"></textarea>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-md-3">Tanggal Lahir</label>
+              <div class="col-md-9">
+                <input name="dob" placeholder="yyyy-mm-dd" class="form-control" type="date">
+              </div>
+            </div>
+          </div>
+        </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" id="btnSave" onclick="save()" class="btn btn-primary">Simpan</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+  <!-- End Bootstrap modal -->
+ 
+  </body>
+</html>
